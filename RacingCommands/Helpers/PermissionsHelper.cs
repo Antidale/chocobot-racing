ef593c8a2d19fb2195a43930bbdfda9308588a20
Antidale <antidale@tellah.life>
2025-06-06 14:06:07 -0700

    add PermissionsHelper class

---
+++ RacingCommands/Helpers/PermissionsHelper.cs
@@ -0,0 +1,14 @@
+namespace chocobot_racing.RacingCommands.Helpers;
+
+public static class PermissionsHelper
+{
+    private static DiscordPermissions BasicPermissions => new DiscordPermissions().Add(DiscordPermission.ViewChannel, DiscordPermission.SendMessages);
+
+    private static DiscordPermissions AdminPermissions => new DiscordPermissions().Add(DiscordPermission.ViewChannel, DiscordPermission.ManageChannels, DiscordPermission.SendMessages, DiscordPermission.ManageMessages);
+
+    public static DiscordOverwriteBuilder GetDenyEveryonePermissionSet(DiscordRole role) => new DiscordOverwriteBuilder(role).Deny(BasicPermissions);
+
+    public static DiscordOverwriteBuilder GetAllowBotAndAdminPermissionSet(DiscordRole role) => new DiscordOverwriteBuilder(role).Allow(AdminPermissions);
+
+    public static DiscordOverwriteBuilder GetAllowUserPermissionSet(DiscordMember member) => new DiscordOverwriteBuilder(member).Allow(BasicPermissions);
+}
