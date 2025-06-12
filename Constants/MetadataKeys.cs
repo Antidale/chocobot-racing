baf1fac50e6d3fb4e7b6c80df7f7ec14ffb0352a
Antidale <antidale@tellah.life>
2025-06-11 17:18:12 -0700

    wip for race logging

---
+++ Constants/MetadataKeys.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace chocobot_racing.Constants;
+
+public class MetadataKeys
+{
+    public const string RoomId = "CR_RoomId";
+    public const string SpoilerRoomId = "CR_SpoilerRoomIdRoomId";
+    public const string AlertMessageId = "CR_AlertMessageId";
+    public const string LeaderboardMessageId = "CR_LeaderboardMessageId";
+    //Future possibility section
+    public const string GuildId = "CR_GuildId";
+    public const string EventId = "CR_EventId";
+}
--- RacingCommands/AsyncRace.cs
+++ RacingCommands/AsyncRace.cs
@@ -1,19 +1,22 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Json;
 using System.Security.Cryptography;
-using System.Threading.Channels;
 using chocobot_racing.Constants;
 using chocobot_racing.Helpers;
 using chocobot_racing.RacingCommands.Helpers;
+using DSharpPlus;
 using DSharpPlus.Commands.Processors.SlashCommands.Metadata;
+using FeInfo.Common.Requests;
 
 namespace chocobot_racing.RacingCommands;
 
 [Command("async"), InteractionInstallType(DiscordApplicationIntegrationType.GuildInstall)]
 [RequireGuild]
-public class AsyncRace(GuildConfigurationHelper configurationHelper)
+public class AsyncRace(GuildConfigurationHelper configurationHelper, FeInfoHttpClient feInfoHttpClient)
 {
     private readonly string alphaNumerics = "abcdefghjkmnpqrstvwxyz1234567890";
+
     [Command("create-async")]
     [Description("creat an async race")]
     public async Task CreateAsyncRaceAsync(
@@ -51,8 +54,8 @@ public class AsyncRace(GuildConfigurationHelper configurationHelper)
 
         //TODO: use configured role
         var everyoneRole = ctx.Guild!.EveryoneRole;
-        var racebotAdminRoleSuccess = ctx.Guild!.Roles.TryGetValue(985344674652885044, out var racebotAdminRole);
-        if (!racebotAdminRoleSuccess)
+        ctx.Guild!.Roles.TryGetValue(985344674652885044, out var racebotAdminRole);
+        if (racebotAdminRole is null)
         {
             await ctx.RespondAsync("could not get racebot admin role");
             return;
@@ -60,54 +63,60 @@ public class AsyncRace(GuildConfigurationHelper configurationHelper)
 
         //TODO: use correct role, currently using Racingway's role in my test server
         var racebotRoleSuccess = ctx.Guild!.Roles.TryGetValue(1380349994053144708, out var racebotRole);
-
-        if (!racebotRoleSuccess)
+        if (racebotRole is null)
         {
             await ctx.RespondAsync("could not get racebot role");
             return;
         }
 
-        var everyonePermissions = PermissionsHelper.GetDenyEveryonePermissionSet(everyoneRole);
-        var botPermissions = PermissionsHelper.GetAllowBotAndAdminPermissionSet(racebotRole!);
-        var adminPermissions = PermissionsHelper.GetAllowBotAndAdminPermissionSet(racebotAdminRole!);
-
-        List<DiscordOverwriteBuilder> permissions = [everyonePermissions, botPermissions, adminPermissions];
+        var roomPermissions = PermissionsHelper.GetStandardRoomPermissions(everyoneRole, racebotRole, racebotAdminRole);
 
         var channelBaseName = $"ff4fe-{RandomNumberGenerator.GetString(alphaNumerics, 6)}-async";
-        var newChannel = await ctx.Guild!.CreateChannelAsync(
+        var raceChannel = await ctx.Guild!.CreateChannelAsync(
             name: channelBaseName,
             type: DiscordChannelType.Text,
             parent: category,
-            overwrites: permissions
-            );
+            overwrites: roomPermissions
+        );
 
-        var message = await newChannel.SendMessageAsync("new race channel!");
-
+        var message = await raceChannel.SendMessageAsync("new race channel!");
         await message.PinAsync();
 
         var spoilerChannel = await ctx.Guild!.CreateChannelAsync(
             name: $"{channelBaseName}-spoilers",
             type: DiscordChannelType.Text,
             parent: category,
-            overwrites: permissions
-            );
+            overwrites: roomPermissions
+        );
 
         var leaderboard = await spoilerChannel.SendMessageAsync("leaderboard post");
         await leaderboard.PinAsync();
-        //TODO: store this message id so that we can update the leaderboard as people finish
 
-        //TODO: sent alert message, which requires moving AlterHelper out a bit, or creating one specifically for asyncs
+        await ctx.RespondAsync($"shiny new channel: {Formatter.Mention(raceChannel)}");
 
-        //todo Log channels created to a database to be able to delete later
-        await ctx.RespondAsync("channel created!");
 
-        var alertMessage = AlertMessageHelper.CreateAsyncAlert(ctx.Member!.DisplayName, description, newChannel.Id, ctx.Guild.Id);
+        //TODO: fix ordering her so it makes sense as things are more finalized
+        var alertMessageBuilder = AlertMessageHelper.CreateAsyncAlert(ctx.Member!.DisplayName, description, raceChannel.Id, ctx.Guild.Id);
         if (!ctx.Guild!.Channels.TryGetValue(alertsChannelId, out var alertsChannel))
         {
             return;
         }
 
-        await alertsChannel.SendMessageAsync(alertMessage);
+        var alertMessage = await alertsChannel.SendMessageAsync(alertMessageBuilder);
+
+        Dictionary<string, string> metadataDict = new()
+        {
+            [MetadataKeys.RoomId] = raceChannel.Id.ToString(),
+            [MetadataKeys.SpoilerRoomId] = spoilerChannel.Id.ToString(),
+            [MetadataKeys.AlertMessageId] = alertMessage.Id.ToString(),
+            [MetadataKeys.LeaderboardMessageId] = leaderboard.Id.ToString()
+        };
+
+        var logCreatedRace = new CreateRaceRoom(ctx.User.Id.ToString(), channelBaseName, "FFA", "discord", metadataDict);
+
+        var apiResponse = await feInfoHttpClient.PostAsJsonAsync("races", logCreatedRace);
+
+        //todo: log info to appropriate internal cache object so we only go to the db for this stuff on startup.
     }
 
     [Command("join")]
@@ -136,8 +145,6 @@ public class AsyncRace(GuildConfigurationHelper configurationHelper)
         }
 
         //TODO: see if the person is already in the race, if they are, let them know but don't do anything else.
-
-
         var overwrites = potentialRoom.PermissionOverwrites.Select(x => DiscordOverwriteBuilder.From(x)).Append(PermissionsHelper.GetAllowUserPermissionSet(member));
 
         await potentialRoom.ModifyAsync((channel) =>
@@ -145,11 +152,9 @@ public class AsyncRace(GuildConfigurationHelper configurationHelper)
             channel.PermissionOverwrites = overwrites;
         });
 
-
         //TODO: Log user joining room to api and/or local db. Make sure we have enough info to send them a DM later when the room closes
         await ctx.RespondAsync($"Join successful! <#{potentialRoom.Id}>");
 
         await potentialRoom.SendMessageAsync($"{member.DisplayName} joined the race!");
-
     }
 }
